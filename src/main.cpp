f2f068e99290723b28c62d6bea26acfad502ab2e
l0calgost <manu.ahrens@gmail.com>
2023-07-27 21:57:48 +0200

    - Use another web library for better async calls - styled the button and used a html template

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <Servo.h>
-#include <ESP8266WebServer.h>
+#include <ESPAsyncWebServer.h>
 #include <config.h>
 
 const char* ssid     = WLAN_SSID;
@@ -8,7 +8,51 @@ const char* password = WLAN_PASSWORD;
 
 Servo servo;
 
-ESP8266WebServer server(80);
+AsyncWebServer server(80);
+
+const char* htmlTemplate = R"(
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Servo Steuerung</title>
+  <style>
+    body { 
+      font-family: Arial, sans-serif;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+    }
+    .button {
+      display: inline-block;
+      padding: 30px 50px;
+      font-size: 25px;
+      text-align: center;
+      text-decoration: none;
+      background-color: #4CAF50;
+      color: #fff;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+    .button:hover {
+      background-color: #45a049;
+    }
+  </style>
+</head>
+<body>
+  <button class='button' onclick='rotateServo()'>Ka-Ching!</button>
+  <script>
+    function rotateServo() {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', '/rotate', true);
+      xhr.send();
+    }
+  </script>
+</body>
+</html>
+)";
 
 // put function declarations here:
 void eject();
@@ -20,23 +64,23 @@ void setup() {
 
   connectWifi();
 
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", htmlTemplate);
+  });
+
+  server.on("/rotate", HTTP_GET, [](AsyncWebServerRequest *request){
+    eject();
+    request->send(200, "text/plain", "Servo wurde um 30° gedreht");
+  });
+  
   servo.attach(2);
   servo.write(180);
 
   server.begin();
-  server.on("/",handleRoot);
-  server.on("/eject", eject);
 }
 
 void loop() {
-  server.handleClient();
-}
-
-void handleRoot() {
-  String message="<h1>Spend some money!!</h1>";
-  message += "<br/><br/>";
-  message += "<a href=\"/eject\">Bling Bling</a>";
-  server.send(200, "text/html", message);
+  // nothing to do
 }
 
 void  eject() {
@@ -44,12 +88,12 @@ void  eject() {
   servo.write(50);
   delay(300);
   servo.write(180);
-  handleRoot();
 }
 
 void connectWifi()
 {
   WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
